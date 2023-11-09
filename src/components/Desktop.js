329c2d1ab234e6e84a2d08b7d75a6fb61ac5c9f8
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2023-11-08 22:06:05 -0800

    finished adding field image

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,52 +1,34 @@
 import styled from 'styled-components'
-import field_img from "../images/field_rectangles.png"
-import num_img from "../images/numbers.png"
-import line_img from "../images/white_yard_lines.png"
+import field_img from "../images/field_image.png"
 
 const ImageContainer = styled.div`
     position: relative;
-    width: 100%;
-    height: auto;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: #5A8124;
 `;
 
 const FieldImage = styled.img`
-    position: absolute;
-    top: 0;
-    left: 0;
+    position: relative;
+    width: 95%;
 `;
 
-const LineImage = styled.img`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-  height: 100%;
-`;
+// const CardContainer = styled.div`
+//     poition: absolute;
+//     width: 20%;
+//     boarder: 10px solid red;
+// `;
 
-const NumImage = styled.img`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-  height: 100%;
-`;
 
 
 const Desktop = () => {
+    console.log(FieldImage.height)
+
     return (
         <ImageContainer>
             <FieldImage
                 src={field_img}
-                // style={{ width: "100%", display: "block" }}
-            />
-            <NumImage
-                src={num_img}
-            />
-            <LineImage
-                src={line_img}
             />
         </ImageContainer>
     )
