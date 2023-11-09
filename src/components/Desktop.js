dde49e46526d076b38b9c4b12064e3c3371319ea
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2023-11-08 23:39:11 -0800

    finished desktop compoents and gave skeleton

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,34 +2,54 @@ import styled from 'styled-components'
 import field_img from "../images/field_image.png"
 
 const ImageContainer = styled.div`
-    position: relative;
-    display: flex;
+    background-color: #5A8124;
     justify-content: center;
     align-items: center;
-    background-color: #5A8124;
+    overflow: hidden;
+    position: relative;
 `;
 
 const FieldImage = styled.img`
-    position: relative;
     width: 95%;
+    z-index: 1;
+    position: relative;
 `;
 
-// const CardContainer = styled.div`
-//     poition: absolute;
-//     width: 20%;
-//     boarder: 10px solid red;
-// `;
+const CardContainer = styled.div`
+    width: 40%;
+    border: 20px solid red;
+    z-index: 2;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    aspect-ratio: 14 / 9;
+    left: ${(props) => (props.index % 2) ? 55 : 5}%;
+    top: ${(props) => ((props.index > 3) ? props.index * 9 + 18 : props.index * 9 + 10)}%; /* Adjust to your desired vertical position */
+    z-index: 1;
+`;
 
 
 
-const Desktop = () => {
-    console.log(FieldImage.height)
+const Desktop = (props) => {
+
+    const articles = [1,2,3,4,5,6,7,8];
 
     return (
         <ImageContainer>
+
             <FieldImage
                 src={field_img}
             />
+
+            {articles.map((element, index) => (
+                <CardContainer element={element} index={index}>
+                    <h2>pass article cards components in {'{element}'}</h2>
+                </CardContainer>
+            ))}
+
         </ImageContainer>
     )
 }
