8c47d312b4a8ccadc00fe12a9f8ab68259358678
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2023-11-12 15:00:39 -0800

    merge fix

---
+++ src/images/players/index.js
@@ -0,0 +1,12 @@
+import player1 from './player1.png'
+import player2 from './player2.png'
+import player3 from './player3.png'
+import player4 from './player4.png'
+import player5 from './player5.png'
+import player6 from './player6.png'
+import player7 from './player7.png'
+import player8 from './player8.png'
+
+const players = [player1, player2, player3, player4, player5, player6, player7, player8]
+
+export default players
